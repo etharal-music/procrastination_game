a377aff11a1f9af1c98dd80abab081e5049b9fb4
etharal-music <23395@nayland.school.nz>
2026-08-05 12:33:18 +1200

    Create procrastination game main page.py
    added main window, button use and genrally started the coding.

---
+++ procrastination game main page.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import ttk
+
+#making the window
+window = tk.Tk()
+window.geometry ('300x150')
+window.title("An Assingment")
+
+#title
+title = ttk.Label(master = window, text = "would you like to:")
+title.pack()
+
+#area where buttons should be
+button_area = ttk.Frame(master=window)
+button_area.pack()
+
+#making the homework button
+homework_button = ttk.Button(master = button_area, text = "homework")
+homework_button.pack(side = 'left', padx= '20', pady = '30')
+
+#making the minigame button
+minigame_button = ttk.Button(master = button_area, text = "minigame!")
+minigame_button.pack(side = 'left', padx= '20', pady = '30')
+
+#run the window
+window.mainloop()
