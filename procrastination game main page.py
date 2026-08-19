a2055a1b65abb360d14aaaa31dd162d6a00d922b
etharal-music <23395@nayland.school.nz>
2026-08-19 12:35:06 +1200

    trying more on minigame and ingame clock
    added an ingame time and worked a bit more on the minigame itself.

--- procrastination game main page.py
+++ procrastination game main page.py
@@ -17,17 +17,21 @@ page_open = 0
 homework_done = 0
 ###########
 
+time_until_homework = 72
 
 #homework command
 
 def homework():
     global homework_done
     global page_open
+    global time_until_homework
+    #this check if there is another page open, if so it does not allow another to open
     if page_open == 0:
         homework_window = tk.Tk()
         homework_window.geometry ('300x150')
         homework_window.title("homework")
         page_open += 1
+        #deletes the main window for now
         window.withdraw()
         #homework progress bar
         Progress_bar = ttk.Progressbar( master = homework_window, length = 200)
@@ -36,16 +40,23 @@ def homework():
         def load_bar():
             global homework_done
             global page_open
+            global time_until_homework
             print(Progress_bar['value'])
+            #if the progres bar is not complete: add one to it and wait a second
             if Progress_bar['value'] < 100:
                 Progress_bar['value'] += 1
 
                 homework_window.after(500, load_bar)
             else:
+            #if the progress bar is complete, take away 6 hours from the ingame clock, allow page acess, respawn main window aswell as delete this one and make the homework
+            #10 % done
                 homework_done += 10
                 page_open -= 1
+                time_until_homework -= 6
                 homework_window.withdraw()
                 window.deiconify()
+                thing()
+                
 
         load_bar()
 
@@ -60,6 +71,7 @@ def homework():
 
 #minigame command
 def minigame():
+    global time_until_homework
     global page_open
     global minigame_1_done
     global checkboxes_clicked
@@ -113,6 +125,7 @@ def minigame():
         checkbox_12 = ttk.Checkbutton(master = minigame_1_window, variable = tickbox_bar12)
         checkbox_12.pack(side = 'right')
         def checkbox_check():
+            global time_until_homework
             global checkboxes_clicked
             global minigame_1_done
             if checkboxes_clicked < 10:
@@ -135,19 +148,30 @@ window = tk.Tk()
 window.geometry('300x150')
 window.title("An Assingment")
 
+
+#label to tell the user how long until the homework is due
+
+def thing():
+    time_until_homework_label = ttk.Label(master = window, text = time_until_homework)
+    time_until_homework_label.pack_forget()
+    time_until_homework_label.pack()
+thing()
+
+
 #title
 title = ttk.Label(master = window, text = "would you like to:")
 title.pack()
+
 
 #area where buttons should be
 button_area = ttk.Frame(master=window)
 button_area.pack()
 
-#making the homework button
+#the homework button
 homework_button = ttk.Button(master = button_area, text = "homework", command = homework)
 homework_button.pack(side = 'left', padx= '20', pady = '30')
 
-#making the minigame button
+#the minigame button
 minigame_button = ttk.Button(master = button_area, text = "minigame!", command = minigame)
 minigame_button.pack(side = 'left', padx= '20', pady = '30')
 
